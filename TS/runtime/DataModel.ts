f0d1b24640d3c49e1d05be2fad8a707ddfb5fce6
huxiaofeng <1129429605@qq.com>
2016-08-10 11:03:21 +0800

    TS实现DataModel

--- TS/runtime/DataModel.ts
+++ TS/runtime/DataModel.ts
@@ -1,140 +1,48 @@
 import {HashMap} from "../lib/HashMap";
-import {Context} from "../runtime/Context";
+//import {Context} from "../runtime/Context";
 
-//import {BindValue} from 
 
-class DataModel extends AbstractObservable{
+class DataModel{
 
-	result: Array<string>;
-	
-	public getResult(){
-		return result;
-	}
-	
-	public setResult(result: Array<string>){
-		this.result = result;
-	}
-	
-	public addPrefix(prefix: string,oldList: Array<string>): Array<string> 
-    {	
-    	
-       	result:Array<string> = new Array<string>(oldList.size());
-        for (var old in oldList)
-        {
-            result.add(prefix + "." + old);
-        }
-        return result;
-    }
-    public create(context: Context):DataModel 
-    {
-        realm:Realm = context.get('DataModelRealm');
-        dataModel:DataModel = new DataModel(realm);
-        dataModel.root = true;
-        return dataModel;
-    }
-    protected transient backup:DataModel;
-
-    private  createTime:number = System.currentTimeMillis();
-
-    /**
-     * 由Realm控制住并发访问，无需使用ConcurrentHashMap
-     */
+    
     private  memberMap:HashMap = new HashMap();
 
-    /**
-     * 根数据模型，即放在上下文里数据模型，而不是放在数据模型里的子数据模型
-     */
-    private  root:boolean;
 
-    /**
-     * 表示是否属于共享数据模型
-     */
-    private  isShared:boolean = false;
-
-    private  stale:boolean;
-
-    private  version:number;
-
-    //private EnumMap enumMap = new EnumMap();
-
-    /**
-     * 内部创建的DataModel不具备自动备份能力，例如子DataModel和备份DataModel
-     * 
-     * @param realm
-     *        Realm
-     */
-    public constructor(realm: Realm)
-    {
-        super(realm);
+    public constructor()
+    { 
     }
     
-
-	/**
-     * 
-     */
-    protected createSimpleMember(memberName: string): BindValue<Object> 
+    protected createSimpleMember(memberName: string): Object
     {
-        checkRealm();
         // 1. create
-        var result = new BindValue<Object>(getRealm());
+        var result = new Object();
         memberMap.put(memberName, result);
         //
         return result;
     }
    
-      /**
-     * 
-     */
     protected createSubModelMember(memberName: string): DataModel 
     {
-        // 0. container
-        member:BindValue<Object> = createSimpleMember(memberName);
-        // 1. create
-        subModel:DataModel = new DataModel(getRealm());
-        // 2. hook
-        member.set(subModel);
-        // $.
+        
+        subModel:DataModel = (DataModel) createSimpleMember(memberName);
         return subModel;
     }
     
     
      private doGet(memberName:string): Object 
     {
-        checkRealm();
-        candidate::BindValue<Object> = getMember(memberName);
+        candidate:Object = getMember(memberName);
         if (candidate == null)
         {
             return null;
         }
-        return candidate.get();
+        return candidate;
     }
     
-    private doGetAllMemberNames(): Array<string> 
-    {
-        checkRealm();
-        Array<String> result = new Array<string>();
-        Array<String> tmp = new Array<string>(memberMap.keySet());
-        //Collections.sort(tmp);
-        for (var key in tmp)
-        {
-            memberContent: Object = get(key);
-            if (memberContent instanceof DataModel)
-            {
-                DataModel subModel = (DataModel) memberContent;
-                result.addAll(addPrefix(key, subModel.getAllMemberNames()));
-            } else if (memberContent != null)
-            {
-                result.add(key);
-            }
-        }
-        return result;
-    }
     
     private doGetMember(memberName: string,
-            create: boolean): BindValue<Object>
+            create: boolean): Object
     {
-
-        checkRealm();
         dotIndex: number = memberName.indexOf('.');
         if (dotIndex > 0)
         {
@@ -151,109 +59,40 @@ class DataModel extends AbstractObservable{
             }
             return subModel.getMember(suffix, create);
         }
-        result: BindValue<Object> = memberMap.get(memberName);
+        result:Object = memberMap.get(memberName);
         if (create && result == null)
         {
-            result = createSimpleMember(memberName);
+            return this;
         }
         return result;
     }
     
-     private doRemove(memberName: string): void
-    {
-        checkRealm();
-        dotIndex: number = memberName.indexOf('.');
-        if (dotIndex > 0)
-        {
-            prefix: string = memberName.substring(0, dotIndex);
-            suffix: string = memberName.substring(dotIndex + 1);
-            subModel:DataModel = (DataModel) get(prefix);
-            if (subModel == null)
-            {
-                return;
-            }
-            subModel.remove(suffix);
-            return;
-        }
-        value: BindValue<Object> = memberMap.remove(memberName);
-        if (value != null)
-        {
-            value.dispose();
-        }
-    }
-    
      private doSet(memberName: string,memberContent: Object): void
     {
-        checkRealm();
-
-        value: BindValue<Object>  = getMember(memberName, true);
-        /*
-        if (memberContent instanceof Collection)
-        {
-            memberContent = wrapCollection(memberName,
-                    (Collection) memberContent);
-
-        }
-        if (memberContent instanceof Map)
-        {
-            memberContent = wrapMap(memberName, (Map) memberContent);
-        }
-        */
-        value.set(memberContent);
+        dm: DataModel  = (DataModel)getMember(memberName, true);
+        dm.memberMap.put(memberName, memberContent);
 
     }
     
-    /**
-     * 获取要素
-     * 
-     * @param memberName
-     *        String
-     * @return Object
-     */
     public get(final memberName: string): Object
     {
         
-        this.result = doGet(memberName);
-        setResult(result);
-        return getResult();
+        return doGet(memberName);
+        
     }
     
-     /**
-     * 
-     */
-    public getAllMemberNames(): Array<string>
-    {
-
-        this.result = doGetAllMemberNames();
-        setResult(result);
-        return getResult();
-    }
-    
-    public getCreateTime():number
-    {
-        return createTime;
-    }
-
-    public getMember(memberName:string ): BindValue<Object>
+    public getMember(memberName:string ): Object
     {
         return getMember(memberName, false);
     }
 
     public getMember(final memberName: string,
-            final create: boolean): BindValue<Object>
+            final create: boolean): Object
     {
-        this.result = doGetMember(memberName, create);
-        setResult(result);
-        return getResult();
+        return doGetMember(memberName, create);
+     
     }
     
-     /**
-     * 
-     * @param memberName
-     *        String
-     * @param memberContent
-     *        String
-     */
     public set(final memberName: string, final memberContent: Object): void
     {
         doSet(memberName, memberContent);
